e9efc092e7641acb70b6db79f4e6272070abe5ab
Julia <Julia@Yulias-MacBook-Air.local>
2023-03-10 15:42:44 +0300

    Created project with base coordinator

---
+++ TestApp/Flows/BaseCoordinator.swift
@@ -0,0 +1,29 @@
+//
+//  BaseCoordinator.swift
+//  TestApp
+//
+//  Created by Yulia Ignateva on 10.03.2023.
+//
+
+import Foundation
+
+class BaseCoordinator {
+    var childCoordinators = [BaseCoordinator]()
+    var onFinish: (() -> ())?
+    
+    func addChildCoordinator(_ coordinator: BaseCoordinator) {
+        childCoordinators.append(coordinator)
+    }
+    
+    func removeChildCoordinator(_ coordinator: BaseCoordinator) {
+        for index in 0..<childCoordinators.count {
+            if childCoordinators[index] === coordinator {
+                childCoordinators.remove(at: index)
+                break
+            }
+        }
+    }
+    
+    func start() {
+    }
+}
